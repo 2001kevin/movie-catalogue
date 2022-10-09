6730610e606f10dd44ea8b7b87d4a29957c5f3e4
2001kevin <silahisabungan7@gmail.com>
2022-10-09 15:26:03 +0800

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: "dbf9b3670bd7b8762aeae2403a303394",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+  DEFAULT_LANGUAGE: "en-us",
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
